06187574fa56438268c57748cb73980eab942e37
P, Shashank <shashank.p@tektronix.com>
2024-09-20 11:43:01 +0530

    fix: updated acq_filters.py in docs additional examples.

--- docs/additional_examples_and_pseudo_code/acq_filters.py
+++ docs/additional_examples_and_pseudo_code/acq_filters.py
@@ -1,12 +1,26 @@
-def any_horizontal_change(previous_header, current_header):
-    """Prebuilt acq acceptance filter that accepts only acqs with
-    changes to horizontal settings.
+from typing import Dict
+
+from tekhsi import WaveformHeader
+
+
+def any_horizontal_change(
+    previous_header: Dict[str, WaveformHeader],
+    current_header: Dict[str, WaveformHeader],
+) -> bool:
+    """Acq acceptance filter that accepts only acqs with changes to horizontal settings.
+
+    Args:
+        previous_header (dict[str, WaveformHeader]): Previous header dictionary.
+        current_header (dict[str, WaveformHeader]): Current header dictionary.
+
+    Returns:
+        True if the acquisition is accepted, False otherwise.
     """
     for key, cur in current_header.items():
         if key not in previous_header:
             return True
         prev = previous_header[key]
-        if prev is None and cur != None:
+        if prev is None and cur is not None:
             return True
         if prev is not None and (
             prev.noofsamples != cur.noofsamples
